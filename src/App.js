a72c8f310a2d03ec56b7bb658b6db00e534195ee
Jeff Christian <jeffchristian.devfrontend@gmail.com>
2022-09-21 16:31:40 -0300

    updated methods for separating categories and words received from data

--- src/App.js
+++ src/App.js
@@ -22,11 +22,48 @@ const stages = [
 function App() {
 
   const [gameStage, setGameStage] = useState(stages[0].name);
-
   const [words] = useState(wordsList);
+
+  // Choose a category and word at Data
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordandCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words);
+    const category = categories[
+      Math.floor( Math.random() * Object.keys(categories).length
+    )];
+
+    // pick a random word
+    const word = words[category][
+      Math.floor( Math.random() * words[category].length
+    )];
+
+    return {category, word};
+
+  }
 
   // method of play game
   const startGame = () => {
+  
+    // pick word and category
+    const {category, word} = pickWordandCategory();
+
+    // create an array of letters
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log(category, word);
+    console.log(wordLetters);
+
+    // fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
     setGameStage(stages[1].name);
   }
 
--- src/data/words.js
+++ src/data/words.js
@@ -1,3 +1,7 @@
 export const wordsList = {
-    random: ["Conselho", "Ponto", "Trilho", "Pessoal", "Caricatura", "Tinta", "Calcanhar", "Ciclomotor", "Morder", "Buraco", "Pelicano", "Óleo", "Sauna", "Mortal", "Canil", "Tigela", "Limpar", "Nuclear", "Piloto", "Ensinar", "Verme"]
+    animais: ["Papagaio", "Furão", "Rato", "Puma", "Boi", "Cavalo", "Gazela", "Tigre", "Basilisco", "Alpaca", "Galo", "Caranguejo", "Impala", "Glutão", "Carneiro", "Lagarto", "Lhama", "Corça", "Salamandra", "Bezerro", "Porco", "Esquilo", "Tatu", "Ovelha", "Galinha", "Sapo"],
+    cores: ["Ciano", "Azul", "Turquesa", "Alizarina", "Amarelo", "Âmbar", "Ametista", "Aspargo", "Ardósia", "Bege", "Bordô", "Bronze", "Caqui", "Prata", "Preto", "Roxo", "Dourado", "Milho", "Jasmine", "Jambo", "Pêssego", "Vermelho"],
+    frutas: ["Acerola", "Amora","Banana", "Caju", "Cajá", "Coco", "Damasco", "Figo", "Guaraná", "Goiaba", "Jambo", "Jabuticaba", "Kiwi", "Limão", "Maçã", "Tangerina", "Uva"],
+    carros: ["Daihatsu", "Rover", "JPX", "Hyundai", "SHINERAY", "LIFAN", "GEELY", "LOBINI", "Volvo", "Kia", "Matra"],
+    aleatória: ["Conselho", "Ponto", "Trilho", "Pessoal", "Caricatura", "Tinta", "Calcanhar", "Ciclomotor", "Morder", "Buraco", "Pelicano", "Óleo", "Sauna", "Mortal", "Canil", "Tigela", "Limpar", "Nuclear", "Piloto", "Ensinar", "Verme"]
 }
